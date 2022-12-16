76bf010415fc4d02228db2b934aa5e47b6ae1439
Aroon Salotra <aroonsalotra@gmail.com>
2022-12-16 00:59:37 +0000

    finish homepage skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,6 @@ export default function Home() {
         <h1 className="text-8xl">Design</h1>
         <p className="text-2xl py-4">With no compromise</p>
 
-
         <div className="overflow-hidden h-20 m-auto">
 
           <ul className="flex flex-col gap-40 justify-center text-3xl overflow-visible transition-[transform] duration-750 ease-out text-4xl"
@@ -59,45 +58,66 @@ export default function Home() {
 
       </section>
 
-      <div className="border-b-[1rem] border-cyan-200" />
+      <div />
 
-      <section className="flex flex-col gap-40 info">
+      <section className="flex flex-col gap-20 pb-20 info border-y-8 max-w-6xl mx-auto">
 
-        <h1 className="text-3xl py-10"> Your website, your way</h1>
+        <h1 className="text-3xl pt-4">
+          <span className="font-semibold">Your</span> website,
+          <span className="font-semibold"> your </span>
+          way</h1>
 
         <div className="animate-expand">
           <ul className="px-0 flex flex-col justify-between text-2xl sm:px-20 sm:flex-row">
-            <li className="border-2 rounded-lg border-gray-400 p-2 bg-amber-400">
-              Design to your demands <GrUserWorker className="m-auto" /></li>
-            <li className="border-2 rounded-lg border-gray-400 p-2 bg-cyan-400">
-              Maintain website <MdPayment className="m-auto" /></li>
-            <li className="border-2 rounded-lg border-gray-400 p-2 bg-red-400">
-              Payment upon completion <BsFillPencilFill className="m-auto" /></li>
+            <li className="rounded-sm border-gray-400 p-2 shadow-sm shadow-black">
+              Design to your demands <BsFillPencilFill className="m-auto" /></li>
+            <li className="rounded-sm border-gray-400 p-2 shadow-sm shadow-black">
+              Mobile responsive <GrUserWorker className="m-auto" /></li>
+            <li className="rounded-sm border-gray-400 p-2 shadow-sm shadow-black">
+              Payment upon completion <MdPayment className="m-auto" /></li>
           </ul>
         </div>
 
-        <button className="rounded-full border-2 border-black p-2 px-4 text-2xl w-60 m-auto">
+        {/* <button className="rounded-full border-2 border-black p-2 px-4 text-2xl w-60 m-auto">
           <Link href={"/work"} />
           See our work
+        </button> */}
+
+        {/* <button className="btn">
+          Get a quote
+        </button> */}
+
+      </section>
+
+      {/* <section>
+        <h2>Why choose us?</h2>
+      </section> */}
+
+      <section className="max-w-6xl mx-auto">
+        <h2 className="text-3xl py-8">Some of our work</h2>
+        <div className="flex flex-col sm:grid grid-cols-2 gap-12">
+
+          {Work.map(({ title, imgUrl }) => {
+            return <div key={title}
+              className="flex flex-col text-left text-2xl">
+              <h1 className="pl-4 font-semibold">{title}</h1>
+              <Image
+                src={imgUrl}
+                width={512}
+                height={512}
+                alt={`${title} example`}
+              />
+            </div>
+          })}
+        </div>
+
+      </section>
+
+      <div className="pt-20">
+        <button className="btn !text-4xl">
+          Get a quote
         </button>
-
-      </section>
-
-      <section className="flex flex-col sm:grid grid-cols-2 gap-8">
-        {Work.map(({ title, imgUrl }) => {
-          return <div key={title}
-            className="flex flex-col text-left text-2xl">
-            <h1>{title}</h1>
-            <Image
-              src={imgUrl}
-              width={512}
-              height={512}
-              alt=""
-            />
-          </div>
-        })}
-
-      </section>
+      </div>
 
     </>
   )
